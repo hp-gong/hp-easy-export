3c44cc593014d70142615c12d187c8b62b028dc8
hp-gong <hp-gong@users.noreply.github.com>
2021-01-08 14:55:23 -0500

    Delete print2.php

--- print/print2.php
+++
@@ -1,5 +0,0 @@
-<?php // Exit if accessed directly.
-if(! defined('ABSPATH')){exit;}
-
-echo '<input type="button" id="btn_grey2" class="btn_grey2" name="btn_grey2" style="display:inline-block;box-sizing:border-box;margin:5px;text-align:center;text-decoration:none;cursor:pointer;background-color:transparent;-webkit-transition:all .25s ease;-moz-transition:all .25s ease;-ms-transition:all .25s ease;-o-transition:all .25s ease;transition:all .25s ease;font-size:10px;font-size:0.9rem;line-height:15px;line-height:1.5rem;min-width:50px;min-width:5rem;padding:5px 5px;border-color:#666;border-width:1px;border-style:solid;border-radius:2.5px; border-color:#a794a7;background-color:#a794a7;color:#ffffff;" value="Print Preview">';
-?>
