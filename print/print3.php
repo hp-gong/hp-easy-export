c7598371d86d1fbe0f9d88c59659816bd1e32dad
hp-gong <hp-gong@users.noreply.github.com>
2021-01-08 14:59:25 -0500

    Add files via upload

---
+++ print/print3.php
@@ -0,0 +1,6 @@
+<?php // Exit if accessed directly.
+if(! defined('ABSPATH')){exit;}
+
+echo '<script type="text/javascript">$(function(){$("#btn_grey3").printPreview({obj2print:"#printReport3",width:"810"});});</script>';
+echo '<input type="button" id="btn_grey3" class="btn_grey3" name="btn_grey3" style="display:inline-block;box-sizing:border-box;margin:5px;text-align:center;text-decoration:none;cursor:pointer;background-color:transparent;-webkit-transition:all .25s ease;-moz-transition:all .25s ease;-ms-transition:all .25s ease;-o-transition:all .25s ease;transition:all .25s ease;font-size:10px;font-size:0.9rem;line-height:15px;line-height:1.5rem;min-width:50px;min-width:5rem;padding:5px 5px;border-color:#666;border-width:1px;border-style:solid;border-radius:2.5px; border-color:#a794a7;background-color:#a794a7;color:#ffffff;" value="Print Preview">';
+?>
